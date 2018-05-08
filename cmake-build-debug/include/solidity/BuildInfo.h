14daeb344cae317f3438b587b106da99597afcb6
AlexAtShyft <alex@shyft.network>
2018-05-08 17:10:34 -0400

    Corrected the Docker username to be all lower caseunix

--- cmake-build-debug/include/solidity/BuildInfo.h
+++ cmake-build-debug/include/solidity/BuildInfo.h
@@ -1,12 +1,12 @@
 #pragma once
 
 #define ETH_PROJECT_VERSION "0.4.21"
-#define SOL_COMMIT_HASH "21232ed4.mod"
+#define SOL_COMMIT_HASH "c07ff5dc.mod"
 #define ETH_BUILD_TYPE "Debug"
 #define ETH_BUILD_OS "Linux"
 #define ETH_BUILD_COMPILER "g++"
 #define ETH_BUILD_PLATFORM "Linux.g++"
-#define SOL_VERSION_PRERELEASE "develop.2018.5.7"
-#define SOL_VERSION_BUILDINFO "commit.21232ed4.mod.Linux.g++"
-#define SOL_VERSION_COMMIT "commit.21232ed4.mod"
+#define SOL_VERSION_PRERELEASE "develop.2018.5.8"
+#define SOL_VERSION_BUILDINFO "commit.c07ff5dc.mod.Linux.g++"
+#define SOL_VERSION_COMMIT "commit.c07ff5dc.mod"
 #define SOL_VERSION_PLATFORM ""
