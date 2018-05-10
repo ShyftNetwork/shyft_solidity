1a140e8f02194f6d92a1e16791908a1222cfde87
AlexAtShyft <alex@shyft.network>
2018-05-10 09:32:53 -0400

    Added prerelease.txt in the root directory of Solidity, ran the build.sh and tested Solc. It is now a Release version

--- cmake-build-debug/include/solidity/BuildInfo.h
+++ cmake-build-debug/include/solidity/BuildInfo.h
@@ -1,12 +1,12 @@
 #pragma once
 
 #define ETH_PROJECT_VERSION "0.4.21"
-#define SOL_COMMIT_HASH "14daeb34.mod"
+#define SOL_COMMIT_HASH "91075539.mod"
 #define ETH_BUILD_TYPE "Debug"
 #define ETH_BUILD_OS "Linux"
 #define ETH_BUILD_COMPILER "g++"
 #define ETH_BUILD_PLATFORM "Linux.g++"
-#define SOL_VERSION_PRERELEASE "develop.2018.5.9"
-#define SOL_VERSION_BUILDINFO "commit.14daeb34.mod.Linux.g++"
-#define SOL_VERSION_COMMIT "commit.14daeb34.mod"
+#define SOL_VERSION_PRERELEASE ""
+#define SOL_VERSION_BUILDINFO "commit.91075539.mod.Linux.g++"
+#define SOL_VERSION_COMMIT "commit.91075539.mod"
 #define SOL_VERSION_PLATFORM ""
