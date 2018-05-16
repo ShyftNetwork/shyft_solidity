88e65e106787a4eb5562dbc69689435811cf810a
AlexAtShyft <alex@shyft.network>
2018-05-16 17:30:33 -0400

    Added sudo ls -asl in install_deps.sh

--- scripts/install_deps.sh
+++ scripts/install_deps.sh
@@ -325,8 +325,9 @@ case $(uname -s) in
                 esac
 
 		echo " Before sudo apt-get -y update"
-                sudo apt-get -y update
+		sudo ls -asl
 		echo " After sudo apt-get -y update"
+                sudo apt-get -y update
                 sudo apt-get -y install \
                     build-essential \
                     cmake \
