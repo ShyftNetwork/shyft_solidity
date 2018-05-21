b077a247ff1af21dc9ec9c92ec3edc3308f544f6
David Ansermino <david@chainsafe.io>
2018-05-20 21:49:19 -0400

    Updated docker deployment

--- scripts/docker_deploy.sh
+++ scripts/docker_deploy.sh
@@ -6,16 +6,16 @@ docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD";
 version=$($(dirname "$0")/get_version.sh)
 if [ "$TRAVIS_BRANCH" = "develop" ]
 then
-    docker tag ethereum/solc:build ethereum/solc:nightly;
-    docker tag ethereum/solc:build ethereum/solc:nightly-"$version"-"$TRAVIS_COMMIT"
-    docker push ethereum/solc:nightly-"$version"-"$TRAVIS_COMMIT";
-    docker push ethereum/solc:nightly;
+    docker tag alexatshyft/solc:build alexatshyft/solc:nightly;
+    docker tag alexatshyft/solc:build alexatshyft/solc:nightly-"$version"-"$TRAVIS_COMMIT"
+    docker push alexatshyft/solc:nightly-"$version"-"$TRAVIS_COMMIT";
+    docker push alexatshyft/solc:nightly;
 elif [ "$TRAVIS_TAG" = v"$version" ]
 then
-    docker tag ethereum/solc:build ethereum/solc:stable;
-    docker tag ethereum/solc:build ethereum/solc:"$version";
-    docker push ethereum/solc:stable;
-    docker push ethereum/solc:"$version";
+    docker tag alexatshyft/solc:build alexatshyft/solc:stable;
+    docker tag alexatshyft/solc:build alexatshyft/solc:"$version";
+    docker push alexatshyft/solc:stable;
+    docker push alexatshyft/solc:"$version";
 else
     echo "Not publishing docker image from branch $TRAVIS_BRANCH or tag $TRAVIS_TAG"
 fi
