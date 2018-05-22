c8502ef491202820caf2b632c7b2bff353721c96
David Ansermino <david@chainsafe.io>
2018-05-22 13:37:40 -0400

    Updated docker repos

--- scripts/docker_deploy.sh
+++ scripts/docker_deploy.sh
@@ -6,16 +6,16 @@ docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD";
 version=$($(dirname "$0")/get_version.sh)
 if [ "$TRAVIS_BRANCH" = "develop" ]
 then
-    docker tag alexatshyft/solc:build alexatshyft/solc:nightly;
-    docker tag alexatshyft/solc:build alexatshyft/solc:nightly-"$version"-"$TRAVIS_COMMIT"
-    docker push alexatshyft/solc:nightly-"$version"-"$TRAVIS_COMMIT";
-    docker push alexatshyft/solc:nightly;
+    docker tag shyftnetwork/solc:build shyftnetwork/solc:nightly;
+    docker tag shyftnetwork/solc:build shyftnetwork/solc:nightly-"$version"-"$TRAVIS_COMMIT"
+    docker push shyftnetwork/solc:nightly-"$version"-"$TRAVIS_COMMIT";
+    docker push shyftnetwork/solc:nightly;
 elif [ "$TRAVIS_TAG" = v"$version" ]
 then
-    docker tag alexatshyft/solc:build alexatshyft/solc:stable;
-    docker tag alexatshyft/solc:build alexatshyft/solc:"$version";
-    docker push alexatshyft/solc:stable;
-    docker push alexatshyft/solc:"$version";
+    docker tag shyftnetwork/solc:build shyftnetwork/solc:stable;
+    docker tag shyftnetwork/solc:build shyftnetwork/solc:"$version";
+    docker push shyftnetwork/solc:stable;
+    docker push shyftnetwork/solc:"$version";
 else
     echo "Not publishing docker image from branch $TRAVIS_BRANCH or tag $TRAVIS_TAG"
 fi
