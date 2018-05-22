61a3027ce7c964465e825c57621c562fa71ac782
David Ansermino <david.ansermino@mail.utoronto.ca>
2018-05-22 14:55:10 -0400

    Fixed repo names

--- scripts/docker_build.sh
+++ scripts/docker_build.sh
@@ -3,6 +3,6 @@
 set -e
 
 docker build -t shyftnetwork/shyft_solidity:build -f scripts/Dockerfile .
-tmp_container=$(docker create ethereum/solc:build sh)
+tmp_container=$(docker create shyftnetwork/shyft_solidity:build sh)
 mkdir -p upload
 docker cp ${tmp_container}:/usr/bin/solc upload/solc-static-linux
