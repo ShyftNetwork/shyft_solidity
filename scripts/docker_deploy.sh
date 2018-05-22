f509de91ccf9f183e9ebc962ca65f23425523d46
David Ansermino <david.ansermino@mail.utoronto.ca>
2018-05-22 15:42:59 -0400

    Fixed repo nombres

--- scripts/docker_deploy.sh
+++ scripts/docker_deploy.sh
@@ -6,16 +6,16 @@ docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD";
 version=$($(dirname "$0")/get_version.sh)
 if [ "$TRAVIS_BRANCH" = "develop" ]
 then
-    docker tag shyftnetwork/solc:build shyftnetwork/solc:nightly;
-    docker tag shyftnetwork/solc:build shyftnetwork/solc:nightly-"$version"-"$TRAVIS_COMMIT"
-    docker push shyftnetwork/solc:nightly-"$version"-"$TRAVIS_COMMIT";
-    docker push shyftnetwork/solc:nightly;
+    docker tag shyftnetwork/shyft_solidity:build shyftnetwork/shyft_solidity:nightly;
+    docker tag shyftnetwork/shyft_solidity:build shyftnetwork/shyft_solidity:nightly-"$version"-"$TRAVIS_COMMIT"
+    docker push shyftnetwork/shyft_solidity:nightly-"$version"-"$TRAVIS_COMMIT";
+    docker push shyftnetwork/shyft_solidity:nightly;
 elif [ "$TRAVIS_TAG" = v"$version" ]
 then
-    docker tag shyftnetwork/solc:build shyftnetwork/solc:stable;
-    docker tag shyftnetwork/solc:build shyftnetwork/solc:"$version";
-    docker push shyftnetwork/solc:stable;
-    docker push shyftnetwork/solc:"$version";
+    docker tag shyftnetwork/shyft_solidity:build shyftnetwork/shyft_solidity:stable;
+    docker tag shyftnetwork/shyft_solidity:build shyftnetwork/shyft_solidity:"$version";
+    docker push shyftnetwork/shyft_solidity:stable;
+    docker push shyftnetwork/shyft_solidity:"$version";
 else
     echo "Not publishing docker image from branch $TRAVIS_BRANCH or tag $TRAVIS_TAG"
 fi
