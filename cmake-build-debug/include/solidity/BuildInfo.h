c7a1403c5da9dd5796f4b3ef6bf2f365742abf8d
AlexAtShyft <alex@shyft.network>
2018-05-29 11:48:51 -0400

    Took out the test flag for Pragma substance

--- cmake-build-debug/include/solidity/BuildInfo.h
+++ cmake-build-debug/include/solidity/BuildInfo.h
@@ -1,12 +1,12 @@
 #pragma once
 
 #define ETH_PROJECT_VERSION "0.4.21"
-#define SOL_COMMIT_HASH "66ac912c.mod"
+#define SOL_COMMIT_HASH "d184c359"
 #define ETH_BUILD_TYPE "Debug"
 #define ETH_BUILD_OS "Linux"
 #define ETH_BUILD_COMPILER "g++"
 #define ETH_BUILD_PLATFORM "Linux.g++"
 #define SOL_VERSION_PRERELEASE ""
-#define SOL_VERSION_BUILDINFO "commit.66ac912c.mod.Linux.g++"
-#define SOL_VERSION_COMMIT "commit.66ac912c.mod"
+#define SOL_VERSION_BUILDINFO "commit.d184c359.Linux.g++"
+#define SOL_VERSION_COMMIT "commit.d184c359"
 #define SOL_VERSION_PLATFORM ""
